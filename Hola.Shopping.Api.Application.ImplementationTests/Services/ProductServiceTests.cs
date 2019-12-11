981c075708b0b2940876ab2191bcc7d027f51795
Joaquín Sosa Martín <jossoma15@hotmail.com>
2019-12-11 15:59:07 +0100

    add samples unit test to the controller and service

---
+++ Hola.Shopping.Api.Application.ImplementationTests/Services/ProductServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Hola.Shopping.Api.Application.Contracts.Services;
+using Hola.Shopping.Api.Application.Dtos;
+using Hola.Shopping.Api.Application.Implementation.Services;
+using Hola.Shopping.Api.Data.Contracts;
+using Hola.Shopping.Api.Data.Contracts.Repositories;
+using Hola.Shopping.Api.Domain.Model;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Hola.Shopping.Api.Application.ImplementationTests.Services
+{
+    [TestFixture]
+    public class ProductServiceTests
+    {
+        Mock<ILogger<ProductService>> _loggerMock;
+        Mock<IMapper> _mapperMock;
+        private Mock<IUnitOfWork> _unitOfWorkMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<ProductService>>();
+            _mapperMock = new Mock<IMapper>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+
+        }
+
+
+        [Test]
+        public void ProductServiceConstructorTest()
+        {
+            // Arrange & Act
+            var service = new ProductService(_unitOfWorkMock.Object, _mapperMock.Object, _loggerMock.Object);
+
+            // Assert
+            service.Should().NotBeNull();
+        }
+
+        [Test]
+        public async Task GetAllProductsSuccessTest()
+        {
+            // Arrange
+            var productEntityList = new List<Product> { new Product(), new Product() };
+            var productList = new List<ProductDto> { new ProductDto(), new ProductDto() };
+
+            var productRepositoryMock = new Mock<IProductRepository>();
+            productRepositoryMock.Setup(repository => repository.GetAll())
+                .Returns(async () => await Task.FromResult(productEntityList));
+
+            _unitOfWorkMock.Setup(x => x.ProductRepository).Returns(productRepositoryMock.Object);
+
+            _mapperMock.Setup(mapper => mapper.Map<IList<Product>, IList<ProductDto>>(It.IsAny<IList<Product>>()))
+                .Returns(productList);
+
+            var service = new ProductService(_unitOfWorkMock.Object, _mapperMock.Object, _loggerMock.Object);
+
+            // Act
+            var response = await service.GetAll();
+
+            // Assert
+            response.Should().NotBeNull();
+            response.GetType().Should().Be(typeof(List<ProductDto>));
+            response.Should().NotBeNull().And.HaveCount(2);
+        }
+    }
+}
---
+++ Hola.Shopping.ApiTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+using Moq;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
+using Hola.Shopping.Api.Application.Contracts.Services;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Hola.Shopping.Api.Application.Dtos;
+using AutoWrapper.Wrappers;
+using Hola.Shopping.Api.Model;
+
+namespace Hola.Shopping.Api.Controllers.Tests
+{
+    [TestFixture]
+    public class ProductControllerTests
+    {
+        Mock<ILogger<ProductController>> _loggerMock;
+        Mock<IMapper> _mapperMock;
+        Mock<IProductService> _productServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+           _loggerMock = new Mock<ILogger<ProductController>>();
+           _mapperMock = new Mock<IMapper>();
+           _productServiceMock = new Mock<IProductService>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            
+        }
+
+
+        [Test]
+        public void ProductControllerConstructorTest()
+        {
+            // Arrange & Act
+            var controller = new ProductController(_loggerMock.Object, _mapperMock.Object, _productServiceMock.Object);
+
+            // Assert
+            controller.Should().NotBeNull();
+        }
+
+        [Test]
+        public async Task GetAllProductsSuccessTest()
+        {
+            // Arrange
+            var productList = new List<ProductDto> { new ProductDto(), new ProductDto() };
+            _productServiceMock.Setup(x => x.GetAll()).Returns(async () => await Task.FromResult(productList));
+
+
+            var controller = new ProductController(_loggerMock.Object, _mapperMock.Object, _productServiceMock.Object);
+            
+            // Act
+            var response = await controller.GetAllProducts();
+
+            // Assert
+            response.Result.Should().NotBeNull();
+            response.GetType().Should().Be(typeof(ApiResponse));
+
+            var list = response.Result as IList<ProductDto>;
+            list.Should().NotBeNull().And.HaveCount(2);
+        }
+
+        [Test]
+        public async Task GetAllProductsThrowExceptionTest()
+        {
+            // Arrange
+            var productList = new List<ProductDto> { new ProductDto(), new ProductDto() };
+            _productServiceMock.Setup(x => x.GetAll()).Throws(new JoaquinServiceException("Service exception"));
+
+            var controller = new ProductController(_loggerMock.Object, _mapperMock.Object, _productServiceMock.Object);
+
+            // Act & Assert
+            Assert.That(async () => await controller.GetAllProducts(), Throws.TypeOf<JoaquinServiceException>());
+        }
+
+        [Test]
+        public async Task GetProductsPagedSuccessTest()
+        {
+            // Arrange
+            var productList = new List<ProductDto> { new ProductDto(), new ProductDto(), new ProductDto(), new ProductDto(), new ProductDto(), new ProductDto() };
+            var responseList = new GenericCollectionResponse<IList<ProductDto>>()
+            {
+                Result = productList,
+                TotalRecords = 6
+            };
+
+            _productServiceMock.Setup(x => x.GetPaged(It.IsAny<ProductPagedDto>())).Returns(async () => await Task.FromResult(responseList));
+
+            var controller = new ProductController(_loggerMock.Object, _mapperMock.Object, _productServiceMock.Object);
+
+            // Act
+            var response = await controller.GetProductsPaged(new GetProductsPagedRequest());
+
+            // Assert
+            response.Result.Should().NotBeNull();
+            response.GetType().Should().Be(typeof(ApiResponse));
+
+            var result = response.Result as GenericCollectionResponse<IList<ProductDto>>;
+            result.Result.Should().NotBeNull().And.HaveCount(6);
+            result.TotalRecords.Should().Be(6);
+        }
+
+        [Test]
+        [Ignore("")]
+        public void InsertProductTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        [Ignore("")]
+        public void UpdateProductTest()
+        {
+            Assert.Fail();
+        }
+    }
+
+    [Serializable]
+    public class JoaquinServiceException : Exception
+    {
+        public JoaquinServiceException(string message) : base(message)
+        {
+        }
+    }
+}
